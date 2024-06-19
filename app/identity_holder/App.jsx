6030a62896bd57cb655c835dbf9dab28cc1a80c9
chanipal <z5361235@ad.unsw.edu.au>
2024-06-20 00:50:55 +1000

    Add navigation bottom bar and create dummy screens.

--- app/identity_holder/App.jsx
+++ app/identity_holder/App.jsx
@@ -1,5 +1,55 @@
-export default function App() {
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import SettingsScreen from "./screens/SettingsScreen";
+import NotificationsScreen from "./screens/NotificationsScreen";
+import WalletScreen from "./screens/WalletScreen";
+
+const Tab = createBottomTabNavigator();
+
+const renderIconByName = (name) => {
+  return ({ color, size }) => (
+    <MaterialCommunityIcons
+      name={name}
+      color={color}
+      size={size}
+    />
+  )
+}
+
+export default App = () => {
   return (
-    <></>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarShowLabel: false,
+          headerShown: false,
+        }}
+        backBehavior="order"
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ tabBarIcon: renderIconByName("home") }}
+        />
+        <Tab.Screen
+          name="Wallet"
+          component={WalletScreen}
+          options={{ tabBarIcon: renderIconByName("wallet") }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={NotificationsScreen}
+          options={{ tabBarIcon: renderIconByName("bell") }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ tabBarIcon: renderIconByName("cog") }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ app/identity_holder/screens/SettingsScreen.jsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SettingsScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Settings</Text>
+    </SafeAreaView>
+  );
+}
+
+export default SettingsScreen;
