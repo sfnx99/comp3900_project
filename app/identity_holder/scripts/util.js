01d610822c372c4c9694b90dbfac382e5cca331d
chanipal <z5361235@ad.unsw.edu.au>
2024-06-21 20:59:54 +1000

    Add documentation for renderIconByName.

--- app/identity_holder/scripts/util.js
+++ app/identity_holder/scripts/util.js
@@ -1,5 +1,14 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+/**
+ * Renders an icon from MaterialCommunityIcons.
+ * See https://static.enapter.com/rn/icons/material-community.html
+ * for icons.
+ * 
+ * @param {string} name - The name of the icon to render.
+ * @param {*} props - Extra props to add to the button.
+ * @returns a react element with the rendered icon.
+ */
 export const renderIconByName = (name, props=null) => {
   return ({ color, size }) => (
     <MaterialCommunityIcons
