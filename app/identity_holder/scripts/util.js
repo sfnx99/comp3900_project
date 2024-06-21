9881660c7b787b8107aa9e24cb5c14c333e52ced
chanipal <z5361235@ad.unsw.edu.au>
2024-06-21 20:34:13 +1000

    Update renderIconByName function to take props.

--- app/identity_holder/scripts/util.js
+++ app/identity_holder/scripts/util.js
@@ -1,11 +1,12 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export const renderIconByName = (name) => {
+export const renderIconByName = (name, props=null) => {
   return ({ color, size }) => (
     <MaterialCommunityIcons
       name={name}
       color={color}
       size={size}
+      {...props}
     />
-  )
+  );
 }
