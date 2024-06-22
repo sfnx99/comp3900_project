9f2054361c8392f432980a6f1219d786b4b030ee
fadsdiet <fadi.hatu@gmail.com>
2024-06-22 13:58:37 +1000

    Added More Screen Dummy Pages

---
+++ app/identity_holder/screens/EditInfo.jsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const EditInfo = () => (
+  <SafeAreaView>
+    <Text>Edit Info</Text>
+  </SafeAreaView>
+);
+
+export default EditInfo;
