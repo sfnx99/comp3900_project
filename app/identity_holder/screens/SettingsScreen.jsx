a241ca8c21e45f88b50e3b9793d34850f34b92cc
chanipal <z5361235@ad.unsw.edu.au>
2024-06-23 02:55:17 +1000

    Add icons to all settings.

--- app/identity_holder/screens/SettingsScreen.jsx
+++ app/identity_holder/screens/SettingsScreen.jsx
@@ -12,42 +12,52 @@ const SettingsScreen = () => {
       <SettingButton
         text="Notifications"
         onPress={dummyFunctions}
+        icon="bell-outline"
       />
       <SettingButton
         text="Activity Share Preferences"
         onPress={dummyFunctions}
+        icon="key-variant"
       />
       <SettingButton
         text="Select ID Displayed Information"
         onPress={dummyFunctions}
+        icon="lock-outline"
       />
       <SettingButton
         text="Dark Mode"
         onPress={dummyFunctions}
+        icon="theme-light-dark"
       />
       <SettingButton
         text="Share App"
         onPress={dummyFunctions}
+        icon="share-variant-outline"
       />
       <SettingButton
         text="Privacy Policy"
         onPress={dummyFunctions}
+        icon="file-document-outline"
       />
       <SettingButton
         text="Terms and Conditions"
         onPress={dummyFunctions}
+        icon="file-find-outline"
       />
       <SettingButton
         text="Cookie Policy"
         onPress={dummyFunctions}
+        icon="email-outline"
       />
       <SettingButton
         text="Support"
         onPress={dummyFunctions}
+        icon="message-outline"
       />
       <SettingButton
         text="Logout"
         onPress={dummyFunctions}
+        icon="logout"
       />
     </SafeAreaView>
   );
