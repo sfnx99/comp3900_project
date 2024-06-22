1ce4b6f3bf01d73541ae98cea2e7063153bd541f
m-br-g <bguser@Mitchell-PC>
2024-06-22 13:48:35 +1000

    Stub CLI interface for service provider added to index.ts

--- services/service_provider/src/index.ts
+++ services/service_provider/src/index.ts
@@ -1,10 +1,12 @@
+#! /usr/bin/env node
+
 // Basic Webserver. Change as you see fit.
 // The CI/CD pipeline will build from this file (src/index.ts)
 
 /*
 TO RUN:
 Install node.js, then restart your computer
-cd into the identity_holder directory
+cd into the service_provider directory
 $ npm i
 $ npx ts-node src/index.ts 
 */
@@ -24,3 +26,28 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`[server]: Service Provider is running at http://localhost:${port}`);
 });
+
+/*
+Below is CLI, utilising Commander.js.
+*/
+
+const { Command } = require("commander");
+const figlet = require("figlet");
+
+const program = new Command();
+console.log(figlet.textSync("Service Provider"));
+
+program
+    .version("1.0.0")
+    .description("A CLI used for operating the Service Provider.")
+    .option("-a --add-format [value]", "Add an acceptable credential format")
+    .option("-f --remove-format [value]", "Remove an acceptable credential format")
+    .option("-i --add-issuer [value]", "Add an entry to the list of trusted issuers")
+    .option("-r --remove-issuer [value]", "Remove an entry from the list of trusted issuers")
+    .parse(process.argv);
+
+const options = program.opts();
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
