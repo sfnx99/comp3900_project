203965a5266d8de787abdf84e730b960dc894dab
chanipal <z5361235@ad.unsw.edu.au>
2024-06-24 02:58:30 +1000

    Move button to 'all activity' to home screen.

--- app/identity_holder/screens/HomeScreen.jsx
+++ app/identity_holder/screens/HomeScreen.jsx
@@ -1,25 +1,32 @@
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 import TextButton from '../components/TextButton';
 import CredentialsCarousel from '../components/CredentialsCarousel';
 import ActivityPreview from '../components/ActivityPreview';
 
-const HomeScreen = () => (
-  <SafeAreaView>
-    <Text>Welcome back,</Text>
-    <Text>Jessica</Text>
-    <CredentialsCarousel />
-    <TextButton
-      text="View All Credentials"
-      onPress={() => console.log('Hello World')}
-    />
-    <ActivityPreview />
-    <TextButton
-      text="View All Activity History"
-      onPress={() => console.log('Hello World')}
-      inverted
-    />
-  </SafeAreaView>
-);
+const HomeScreen = ({ credentials, activities }) => {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView>
+      <Text>Welcome back,</Text>
+      <Text>Jessica</Text>
+      <CredentialsCarousel />
+      <TextButton
+        text="View All Credentials"
+        onPress={() => console.log('Hello World')}
+      />
+      <ActivityPreview
+        activities={activities}
+      />
+      <TextButton
+        text="View All Activity History"
+        onPress={() => navigation.navigate('ActivityHistory')}
+        inverted
+      />
+    </SafeAreaView>
+  );
+};
 
 export default HomeScreen;
