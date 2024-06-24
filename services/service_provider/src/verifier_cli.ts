a6d31d19afb66aa8a9abf946d7ab12eaee7f8842
m-br-g <bguser@Mitchell-PC>
2024-06-24 19:07:02 +1000

    fixed trusted.json defaults

--- services/service_provider/src/verifier_cli.ts
+++ services/service_provider/src/verifier_cli.ts
@@ -10,55 +10,84 @@ const program = new Command();
 console.log(figlet.textSync("Service Provider"));
 
 program
-    .version("1.0.0")
+    .version("1.0.1")
     .description("A CLI used for operating the Service Provider.")
-    .option("-a --add", "Add an object when paired with a matching credential/issuer")
-    .option("-r --remove", "Remove an object when paired with a matching issuer name")
+    .option("-r --remove", "Remove an object when paired with a matching credential/issuer name")
     .option("-c --cred [value]", "Specify a credential to be added or removed from the metadata file.")
-    .option("-i --trust [value]", "Specify an issuer to be added or removed from the trusted issuers file.")
+    .option("-i --issuer [value]", "Specify an issuer to be added or removed from the trusted issuers file.")
     .parse(process.argv);
 
 const options = program.opts();
 
-function add_json_object_to_file(new_object: string, filepath: string, style: string): void {
+function add_metadata_to_file(new_entry: string, filepath: string, remove: string): void {
     fs.readFile(filepath, (err: NodeJS.ErrnoException | null, data: Buffer) => {
         if (err) {
             console.error("Could not read file:", err);
             return;
         }
 
-        const jsonObject = JSON.parse(data.toString());
-
-        // try {
-        //     jsonObject = JSON.parse(data.toString());
-        // } catch (parseErr) {
-        //     console.error("Could not parse JSON:", parseErr);
-        //     return;
-        // }
-
-        if (!Array.isArray(jsonObject.list)) {
-            jsonObject.list = [];
+        const json_object = JSON.parse(data.toString());
+        if (!Array.isArray(json_object.formats)) {
+            json_object.formats = [];
         }
 
         // Add or remove the new object
-	    if (style === "add") {
-            const new_json_object = JSON.parse(new_object);
-            jsonObject.list.push(new_json_object);
-            console.log("JSON object added successfully.");
-        } else if (style === "remove") {
-            const index = jsonObject.list.findIndex((item: { iss: string; }) => item.iss === new_object);
+	    if (!remove) {
+            json_object.formats.push(new_entry);
+            console.log("Added successfully.");
+        } else {
+            const index = json_object.formats.indexOf(new_entry.toString());
 
             if (index > -1) {
-                jsonObject.list.splice(index, 1);
-                console.log("JSON object removed successfully.");
+                json_object.formats.splice(index, 1);
+                console.log("Removed successfully.");
             } else {
-                console.error("Object not in list.");
+                console.error("Item not in list.");
                 return;
             }
         }
 
         // Write the updated JSON object back to the file
-        fs.writeFile(filepath, JSON.stringify(jsonObject, null, 2), 'utf8', (writeErr: NodeJS.ErrnoException | null) => {
+        fs.writeFile(filepath, JSON.stringify(json_object, null, 2), 'utf8', (writeErr: NodeJS.ErrnoException | null) => {
+            if (writeErr) {
+                console.error("Could not write file:", writeErr);
+                return;
+            }
+        });
+    });
+}
+
+function add_issuer_to_file(new_entry: string, filepath: string, remove: string): void {
+    fs.readFile(filepath, (err: NodeJS.ErrnoException | null, data: Buffer) => {
+        if (err) {
+            console.error("Could not read file:", err);
+            return;
+        }
+
+        const json_object = JSON.parse(data.toString());
+        if (!Array.isArray(json_object.issuers)) {
+            json_object.issuers = [];
+        }
+
+        // Add or remove the new object
+	    if (!remove) {
+            json_object.issuers.push(new_entry);
+            console.log("Added successfully.");
+        } else {
+            console.log(new_entry)
+            const index = json_object.issuers.indexOf(new_entry);
+
+            if (index > -1) {
+                json_object.issuers.splice(index, 1);
+                console.log("Removed successfully.");
+            } else {
+                console.error("Item not in list.");
+                return;
+            }
+        }
+
+        // Write the updated JSON object back to the file
+        fs.writeFile(filepath, JSON.stringify(json_object, null, 2), 'utf8', (writeErr: NodeJS.ErrnoException | null) => {
             if (writeErr) {
                 console.error("Could not write file:", writeErr);
                 return;
@@ -69,21 +98,11 @@ function add_json_object_to_file(new_object: string, filepath: string, style: st
 
 // Check options provided in arguments and resolve them to corresponding commands.
 if (options.cred) {
-    if ((!options.add && !options.remove) || (options.add && options.remove)) {
-        console.log("Add and remove arguments input incorrectly");
-    } else {
-	const style = options.add === true ? "add" : "remove"
-        add_json_object_to_file(options.cred, "./metadata.json", style)
-    }
+    add_metadata_to_file(options.cred, "./metadata.json", options.remove)
 }
 
-if (options.trust) {
-    if ((!options.add && !options.remove) || (options.add && options.remove)) {
-        console.log("Add and remove arguments input incorrectly");
-    } else {
-        const style = options.add === true ? "add" : "remove"
-        add_json_object_to_file(options.trust, "trusted.json", style)
-    }
+if (options.issuer) {
+    add_issuer_to_file(options.issuer, "./trusted.json", options.remove)
 }
 
 if (!process.argv.slice(2).length) {
