8d988a1edc1ddf65f8047de7e4460d2e99b4ad8e
Alex Blackmore <ablackmore113@gmail.com>
2024-07-08 12:57:19 +1000

    implement v2 wallet endpoints. no testing yet.

--- services/identity_holder/src/auth.ts
+++ services/identity_holder/src/auth.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 import { SALT_ROUNDS, getData, setData, toUser } from './data';
-import { ResponseV2 } from './interface';
+import { ResponseV2, User } from './interface';
 
 export function authRegister(email: string, password: string) : ResponseV2 {
     const data = getData();
@@ -95,6 +95,19 @@ export function authLogout(token: string) {
     user.sessions = user.sessions.filter(e => e != token);
     setData(data);
 
+    return {
+        status: 200,
+        body: {}
+    }
+}
+
+// V2
+
+export function authLogoutV2(user: User, token: string|undefined) {
+    // Invalidate session
+    if (token !== undefined) {
+        user.sessions = user.sessions.filter(e => e != token);
+    }
     return {
         status: 200,
         body: {}
