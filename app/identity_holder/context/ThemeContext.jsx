06ba79a832a35330c3db60cce8cf8ffb41bc2569
chanipal <z5361235@ad.unsw.edu.au>
2024-07-08 23:30:42 +1000

    Save user's preferred theme to device.

--- app/identity_holder/context/ThemeContext.jsx
+++ app/identity_holder/context/ThemeContext.jsx
@@ -1,23 +1,39 @@
 import {
   createContext,
+  useCallback,
   useContext,
+  useEffect,
   useMemo,
   useState,
 } from 'react';
 import PropTypes from 'prop-types';
 
 import { dark, light } from '../styles/themes';
+import { getValueFor, save } from '../scripts/util';
 
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
 
-  const toggleTheme = () => {
-    setDarkMode(!darkMode);
-  };
+  const toggleTheme = useCallback(() => {
+    setDarkMode((prevState) => {
+      const newState = !prevState;
+      save('darkMode', newState ? 'true' : 'false');
+      return newState;
+    });
+  }, []);
 
   const theme = darkMode ? dark : light;
+
+  useEffect(() => {
+    const fetchTheme = async () => {
+      const savedTheme = await getValueFor('darkMode');
+      setDarkMode(savedTheme === 'true');
+    };
+
+    fetchTheme();
+  }, []);
 
   const contextValue = useMemo(() => ({ theme, darkMode, toggleTheme }), [darkMode]);
 
