6351f6472d5cadb3d332e7d8c178fa2c9ee698a3
Dan Field <z5416299@ad.unsw.edu.au>
2024-07-12 09:11:31 +1000

    start of v2

--- services/issuer/src/index.ts
+++ services/issuer/src/index.ts
@@ -14,14 +14,19 @@ import express, { Express, Request, Response } from "express";
 import { metadata } from "./metadata";
 import { authorize } from "./authorize";
 import { issue } from "./issue";
+// @ts-ignore
+import { anchor, DID, generateKeyPair } from '@decentralized-identity/ion-tools';
+// import * as bbs from '@digitalbazaar/bbs-signatures';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8082;
 
+let did_uri = '';
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+    res.json({ did_uri });
 });
 
 app.get("/v1/metadata", (req: Request, res: Response) => {
@@ -39,6 +44,39 @@ app.get("/v1/credential/issue", (req: Request, res: Response) => {
     res.json(issue(access_token));
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`[server]: Issuer Server is running at http://localhost:${port}`);
+    let authKeys = await generateKeyPair();
+    let did = new DID({
+        content: {
+            publicKeys: [
+                {
+                    id: 'key-1',
+                    type: 'EcdsaSecp256k1VerificationKey2019',
+                    publicKeyJwk: authKeys.publicJwk,
+                    purposes: [ 'authentication' ]
+                }
+            ],
+            services: [
+                {
+                    id: 'bbs-key-1',
+                    type: 'BBSKey',
+                    serviceEndpoint: {
+                        "credential_endpoint": "localhost:8082",
+                        "credential_configurations_supported": {
+                            // import these from the cli
+                            "DriverLicenceCredential": {
+                                "format": "ldp_vc"
+                            },
+                            "PhotoCardCredential": {
+                                "format": "ldp_vc"
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    });
+
+    did_uri = await did.getURI();
 });
