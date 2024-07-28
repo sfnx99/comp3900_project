9a4b604e9aa3ce45e889068dc6d3aeb22959cf08
Alex Blackmore <ablackmore113@gmail.com>
2024-07-28 20:22:54 +1000

    fix circular ref

--- services/__tests__/integration.ts
+++ services/__tests__/integration.ts
@@ -1,94 +1,232 @@
-// const { exec } = require('node:child_process'); // eslint-disable-line
-// const { chdir } = require('node:process'); // eslinte-disable-line
-// import axios from "axios";
+const { exec } = require('child_process'); // eslint-disable-line
+const { chdir } = require('node:process'); // eslint-disable-line
+import axios from 'axios';
 
-// let issuer_did = "";
-// const issuer_url = "http://localhost:8081";
-// const wallet_url = "http://localhost:8082";
-// const verifier_url = "http://localhost:8083";
+const issuer_url = "http://localhost:8082";
+const wallet_url = "http://localhost:8081";
+const verifier_url = "http://localhost:8083";
 
-describe('Integration tests', () => {
-    beforeAll(async () => {
-        // // start agents
-        // chdir('issuer');
-        // const iss = exec('docker-compose up');
-        // // extract DID
-        // iss.stdout.on('data', (data: string) => {
-        //     if (data.includes('DID document')) {
-        //         issuer_did = /did:ion:[\w:]+/.exec(data)![0]
-        //         console.log(issuer_did);
-        //     }
-        // });
-        // chdir('..');
-        // chdir('service_provider');
-        // exec('docker-compose up');
-        // chdir('..');
-        // chdir('identity_holder');
-        // exec('docker-compose up');
-        // chdir('..')
-    });
-
+describe('Test integration', () => {
+    beforeAll(() => {
+        chdir('issuer');
+        exec('docker-compose up -d');
+        chdir('..');
+        chdir('service_provider');
+        exec('docker-compose up -d');
+        chdir('..');
+        chdir('identity_holder');
+        exec('docker-compose up -d');
+        chdir('..');
+        return new Promise(r => setTimeout(r, 5000));
+    }, 6000);
+    
     afterAll(() => {
-        // chdir('issuer');
-        // exec('docker-compose down');
-        // chdir('..');
-        // chdir('service_provider');
-        // exec('docker-compose down');
-        // chdir('..');
-        // chdir('identity_holder');
-        // exec('docker-compose down');
-        // chdir('..')
+        chdir('issuer');
+        exec('docker-compose down');
+        chdir('..');
+        chdir('service_provider');
+        exec('docker-compose down');
+        chdir('..');
+        chdir('identity_holder');
+        exec('docker-compose down');
+        return new Promise(r => setTimeout(r, 15000));
+    }, 16000);
+
+    test('Successful flow with user known to issuer', async () => {
+        let res;
+        res = await axios.post(wallet_url + "/v2/auth/register", {
+                email: "bob@test.com",
+                password: "hunter2"
+        });
+        const token = res.data.token;
+
+        expect(token).toBeDefined();
+        expect(res.status).toStrictEqual(200);
+        
+        // retrieve auth code
+        // need to simulate clicking the submit button - we can do this by manually submitting post to verifer
+        res = await axios.post(issuer_url + "/v2/authorize", {
+            client_id: "bob@test.com",
+            client_secret: "wahoo",
+            redirect_uri: wallet_url,
+            state: "xyz",
+            scope: "DriverLicenceCredential"
+        });
+    
+        const auth_code = res.data.code;
+        const state = res.data.state;
+
+        expect(auth_code).toBeDefined();
+        expect(state).toBeDefined();
+        expect(state).toStrictEqual("xyz");
+        expect(res.status).toStrictEqual(200);
+
+        // retrieve issuer DID
+        res = await axios.get(issuer_url);
+    
+        const issuer_did = res.data.did_uri;
+
+        expect(issuer_did).toBeDefined();
+        expect(res.status).toStrictEqual(200);
+
+        // ask to have credential issued
+        res = await axios.post(wallet_url + "/v2/issue", {
+                issuer_id: issuer_did,
+                auth_code: auth_code,
+                redirect_uri: wallet_url,
+                type: "DriverLicenceCredential"
+        },{
+            headers: {
+            Authorization: `Bearer ${token}`
+        }});
+    
+        expect(res.status).toStrictEqual(200);
+    
+        // should receive credential ID
+        const cred_id = res.data.credential_id
+
+        expect(cred_id).toBeDefined();
+    
+        // get presentation metadata
+        res = await axios.get(wallet_url + `/v2/present?verifier_uri=${"http://host.docker.internal:8083"}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    
+        const type = res.data.type;
+        const attrs = res.data.requiredAttributes;
+        expect(type).toBeDefined();
+        expect(attrs).toBeDefined();
+        expect(res.status).toStrictEqual(200);
+
+        // make verifier trust this issuer
+        res = await axios.post(verifier_url + "/v2/trust", {
+            "id": issuer_did
+        });
+
+        expect(res.status).toStrictEqual(200);
+    
+        // perform presentation
+        res = await axios.post(wallet_url + "/v2/present", {
+            verifier_uri: "http://host.docker.internal:8083",
+            credential_id: cred_id
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        expect(res.status).toStrictEqual(200);
     });
 
-    test('Successful flow: hardcoded user (pre-registered to issuer)', async () => {
-        // test still WIP
+    test('Successful flow with user not known to issuer', async () => {
+        let res;
+        res = await axios.post(wallet_url + "/v2/auth/register", {
+                email: "harry@test.com",
+                password: "hunter2"
+        });
+        const token = res.data.token;
 
-        // // register user with wallet
-        // let res;
-        // res = await axios.post(wallet_url + "/v2/auth/register", {
-        //     body: {
-        //         email: "bob@test.com",
-        //         password: "hunter2"
-        //     }
-        // });
-        // const token = res.data.token;
-
-        // expect(token).toBeDefined();
+        expect(token).toBeDefined();
+        expect(res.status).toStrictEqual(200);
         
-        // // retrieve auth code
-        // // need to simulate clicking the submit button - we can do this by manually submitting post to verifer
-        // res = await axios.post(issuer_url + "/v2/authorize", {
-        //     body: {
-        //         client_id: "bob@test.com",
-        //         client_secret: "wahoo",
-        //         redirect_uri: wallet_url,
-        //         state: "xyz",
-        //         scope: "DriverLicenceCredential"
-        //     }
-        // });
+        // need to register with issuer now
+        res = await axios.post(issuer_url + "/v2/register", {
+            email: "harry@test.com",
+            password: "hunter2"
+        });
 
-        // const auth_code = res.data.code
-        // expect(auth_code).toBeDefined();
-        // expect(res.data.state).toBeDefined();
-        // expect(res.data.state).toStrictEqual("xyz");
+        expect(res.status).toStrictEqual(200);
 
-        // // ask to have credential issued
-        // res = await axios.post(wallet_url + "/v2/issue", {
-        //     headers: {
-        //         Authorization: `Bearer ${token}`
-        //     },
-        //     body: {
-        //         issuer_id: issuer_did,
-        //         auth_code: auth_code,
-        //         redirect_uri: wallet_url,
-        //         type: "DriverLicenceCredential"
-        //     }
-        // });
+        // also need to provide information to issuer
+        res = await axios.post(issuer_url + "/v2/info", {
+            email: "harry@test.com",
+            info: {
+                firstName: "Harry",
+                lastName: "Wild",
+                licenseNo: "287",
+                expiryDate: "20/12/2026",
+                dob: "11/09/2001"
+            }
+        });
 
-        // // should receive credential ID
-        // const cred_id = res.data.credential_id
+        expect(res.status).toStrictEqual(200);
 
-        // expect(cred_id).toBeDefined();
+        // now we can retrieve auth code
+        // need to simulate clicking the submit button - we can do this by manually submitting post to verifer
+        res = await axios.post(issuer_url + "/v2/authorize", {
+            client_id: "harry@test.com",
+            client_secret: "wahoo", // this is unused
+            redirect_uri: wallet_url,
+            state: "xyz",
+            scope: "DriverLicenceCredential"
+        });
+    
+        const auth_code = res.data.code;
+        const state = res.data.state;
 
+        expect(auth_code).toBeDefined();
+        expect(state).toBeDefined();
+        expect(state).toStrictEqual("xyz");
+        expect(res.status).toStrictEqual(200);
+    
+        // retrieve issuer DID
+        res = await axios.get(issuer_url);
+    
+        const issuer_did = res.data.did_uri;
+
+        expect(issuer_did).toBeDefined();
+        expect(res.status).toStrictEqual(200);
+
+        // ask to have credential issued
+        res = await axios.post(wallet_url + "/v2/issue", {
+                issuer_id: issuer_did,
+                auth_code: auth_code,
+                redirect_uri: wallet_url,
+                type: "DriverLicenceCredential"
+        },{
+            headers: {
+            Authorization: `Bearer ${token}`
+        }});
+    
+        expect(res.status).toStrictEqual(200);
+    
+        // should receive credential ID
+        const cred_id = res.data.credential_id
+
+        expect(cred_id).toBeDefined();
+    
+        // get presentation metadata
+        res = await axios.get(wallet_url + `/v2/present?verifier_uri=${"http://host.docker.internal:8083"}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    
+        const type = res.data.type;
+        const attrs = res.data.requiredAttributes;
+        expect(type).toBeDefined();
+        expect(attrs).toBeDefined();
+        expect(res.status).toStrictEqual(200);
+
+        // make verifier trust this issuer
+        res = await axios.post(verifier_url + "/v2/trust", {
+            "id": issuer_did
+        });
+
+        expect(res.status).toStrictEqual(200);
+    
+        // perform presentation
+        res = await axios.post(wallet_url + "/v2/present", {
+            verifier_uri: "http://host.docker.internal:8083",
+            credential_id: cred_id
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        expect(res.status).toStrictEqual(200);
     });
 });
