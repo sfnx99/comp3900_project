4ae4d5263853d7ac3bc22adcf46eca524feb6bd8
m-br-g <bguser@Mitchell-PC>
2024-07-29 23:29:25 +1000

    Newgit add -A! New commit yippeegit add -A!

--- services/__tests__/verifier_request.ts
+++ services/__tests__/verifier_request.ts
@@ -2,6 +2,7 @@
 
 import { PresentationDefinition } from "../service_provider/src/interface"
 import { requestMetadata } from "../service_provider/src/request"
+import { modifyDefinition } from "../service_provider/src/db"
 
 describe('Verifier V2: Requests', () => {
     beforeEach(() => {
@@ -11,31 +12,28 @@ describe('Verifier V2: Requests', () => {
         const result = await requestMetadata();
         expect(result.status).toBe(200);
         expect(result.body.state).toBe("smurgle");
-        expect(result.body.presentation_definition).toBe(TEST_PRESENTATION);
+        expect(result.body.presentation_definition.input_descriptors).toStrictEqual(TEST_PRESENTATION);
     })
 
-    test('Test v2/request fails if presentationDefinitions are not present', async ()=> {
-        const result = await requestMetadata();
-        expect(result.status).toBe(500);
-        expect(result.body.message).toBe('Internal Server Error');
-    })
+    // test('Test v2/request fails if presentationDefinitions are unreadable', async ()=> {
+    //     const result = await requestMetadata();
+    //     expect(result.status).toBe(500);
+    //     expect(result.body.message).toBe('Internal Server Error');
+    // })
 })
 
-const TEST_PRESENTATION: PresentationDefinition = {
-    "id": "wah00!",
-    "input_descriptors": [
-        {
-            "id": "DriverLicenceCredential",
-            "constraints": {
-                "fields": [
-                    {
-                        "path": ["$.firstName"]
-                    },
-                    {
-                        "path": ["$.lastName"]
-                    }
-                ]
-            }
-        }
-    ]
-}
+const TEST_PRESENTATION = [
+    {
+        "constraints": {
+            "fields": [
+                {
+                    "path": ["$.firstName"]
+                },
+                {
+                    "path": ["$.lastName"]
+                }
+            ]
+        },
+        "id": "DriverLicenceCredential"
+    }
+]
--- services/jest.config.js
+++ services/jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
+  transformIgnorePatterns: [
+    "service_provider/node_modules/(?!@digitalbazzar)/",
+  ]
 };
