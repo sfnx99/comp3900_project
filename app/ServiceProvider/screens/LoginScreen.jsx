954a81a7b71fe643d27bc9b73863beccaa874438
fadsdiet <fadi.hatu@gmail.com>
2024-07-29 15:52:14 +1000

    Service provider

--- app/ServiceProvider/screens/LoginScreen.jsx
+++ app/ServiceProvider/screens/LoginScreen.jsx
@@ -3,33 +3,118 @@ import {
   View,
   Text,
   Image,
+  TouchableOpacity,
+  Modal,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 import styles from '../styles/loginStyles';
 import Logo from '../images/logo3.png';
+import TextInputField from '../components/TextInputField';
+import { loginUser } from '../scripts/api';
 import TextButton from '../components/TextButton';
+import ErrorMessage from '../components/ErrorMessage';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
+  const [isBiometricSupported, setIsBiometricSupported] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      setIsBiometricSupported(compatible);
+    })();
+  }, []);
+
+  const authenticate = async () => {
+    try {
+      const result = await LocalAuthentication.authenticateAsync({
+        promptMessage: 'Authenticate with Face ID',
+        fallbackLabel: 'Enter Password',
+      });
+
+      if (result.success) {
+        navigation.replace('MainApp');
+      } else {
+        setError('Authentication Failed, Please try again.');
+      }
+    } catch (error) {
+      setError(`Error: ${error.message}`);
+    }
+  };
+
+  const login = async () => {
+    try {
+      if (!email || !password) {
+        setError('Please fill in all fields!');
+        return;
+      }
+
+      await loginUser(email, password);
+      navigation.replace('MainApp');
+    } catch (error) {
+      setError(`Could not login: ${error.message}`);
+    }
+  };
+
+  const clearError = () => {
+    setError('');
+  };
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={Logo}
-        style={styles.logo}
-      />
-      <Text style={styles.text}> BW Credentials</Text>
-      <Text style={styles.subtitletext}> Credential Issuer App</Text>
-
-      <View>
-        <TextButton
-          style={styles.button}
-          text="Login"
-          onPress={() => navigation.replace('MainApp')}
+    <TouchableWithoutFeedback onPress={clearError}>
+      <View style={styles.container}>
+        <Image
+          source={Logo}
+          style={styles.logo}
         />
+        <Text style={styles.text}>BW Credentials</Text>
+
+        <TextInputField
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Email address"
+        />
+        <TextInputField
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Password"
+          isPassword
+        />
+
+        <Modal
+          visible={!!error}
+          transparent={true}
+          animationType="fade"
+        >
+          <ErrorMessage message={error} onPress={clearError} />
+        </Modal>
+
+        <View>
+          <TextButton
+            style={styles.button}
+            text="Login"
+            onPress={login}
+          />
+          {isBiometricSupported && (
+            <TextButton
+              style={styles.buttonInverted}
+              text="Login with Face ID"
+              onPress={authenticate}
+              inverted
+            />
+          )}
+          <TouchableOpacity style={[styles.button, styles.registerButton]} onPress={() => navigation.navigate('Register')}>
+            <Text style={styles.registerText}>Register</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
--- app/ServiceProvider/screens/RequestCredentialScreen.jsx
+++ app/ServiceProvider/screens/RequestCredentialScreen.jsx
@@ -11,7 +11,7 @@ import {
 import { SafeAreaView } from 'react-native-safe-area-context';
 import TextInputField from '../components/TextInputField';
 import TextButton from '../components/TextButton';
-import { getIssuers, PostDefinition } from '../scripts/api'; // Ensure PostDefinition is imported
+import { getIssuers, PostDefinition } from '../scripts/api'; 
 import styles from '../styles/request';
 
 const { width } = Dimensions.get('window');
@@ -20,30 +20,17 @@ const RequestCredentialScreen = ({ navigation }) => {
   const [error, setError] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [IssuerChecked, setIssuerChecked] = useState('');
   const [FnameChecked, setFnameChecked] = useState(false);
   const [LnameChecked, setLnameChecked] = useState(false);
   const [DOBChecked, setDOBChecked] = useState(false);
   const [photocardChecked, setPhotocardChecked] = useState(false);
   const [driversLicenseChecked, setDriversLicenseChecked] = useState(false);
   const [selectedIssuer, setSelectedIssuer] = useState('');
-  const [issuers, setIssuers] = useState([]);
 
   useEffect(() => {
-    loadIssuers();
+    
   }, []);
-
-  const loadIssuers = async () => {
-    try {
-      const result = await getIssuers();
-      if (result && result.issuers) {
-        setIssuers(result.issuers);
-      } else {
-        throw new Error('No issuer data found');
-      }
-    } catch (error) {
-      setError('Failed to fetch issuers: ' + error.message);
-    }
-  };
 
   const UserInfo = async () => {
     if (!email || !password || !selectedIssuer) {
@@ -90,24 +77,19 @@ const RequestCredentialScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContent}>
-        {error ? (
-          <View style={localStyles.errorMessageContainer}>
-            <Text style={localStyles.errorMessage}>{error}</Text>
-          </View>
-        ) : null}
-        <Text style={localStyles.text}>Register User to Receive Credential</Text>
 
-        <TextInputField 
-          value={email}
-          onChangeText={setEmail}
-          placeholder="Email address"
-        />
-        <TextInputField
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Password"
-          isPassword
-        />
+      <Text style={localStyles.text}>Select Issuer to Trust</Text>
+        <View style={localStyles.switchContainer2}>
+          <View style={localStyles.switchRow}>
+          <Switch
+              value={IssuerChecked}
+              onValueChange={setIssuerChecked}
+              trackColor={{ false: '#000', true: '#71d444' }}
+              thumbColor={IssuerChecked ? 'white' : 'white'}
+            />
+            <Text style={localStyles.label}>NSW Government</Text>
+          </View>
+        </View>
 
         <Text style={localStyles.text}>Select Details Required From Credential</Text>
         <View style={localStyles.switchContainer}>
@@ -115,8 +97,8 @@ const RequestCredentialScreen = ({ navigation }) => {
             <Switch
               value={FnameChecked}
               onValueChange={setFnameChecked}
-              trackColor={{ false: '#000', true: '#81b0ff' }}
-              thumbColor={FnameChecked ? '#f5dd4b' : '#f4f3f4'}
+              trackColor={{ false: '#000', true: '#71d444' }}
+              thumbColor={FnameChecked ? 'white' : 'white'}
             />
             <Text style={localStyles.label}>First Name</Text>
           </View>
@@ -124,8 +106,8 @@ const RequestCredentialScreen = ({ navigation }) => {
             <Switch
               value={LnameChecked}
               onValueChange={setLnameChecked}
-              trackColor={{ false: '#000', true: '#81b0ff' }}
-              thumbColor={LnameChecked ? '#f5dd4b' : '#f4f3f4'}
+              trackColor={{ false: 'white', true: '#71d444' }}
+              thumbColor={LnameChecked ? 'white' : 'white'}
             />
             <Text style={localStyles.label}>Last Name</Text>
           </View>
@@ -133,8 +115,8 @@ const RequestCredentialScreen = ({ navigation }) => {
             <Switch
               value={DOBChecked}
               onValueChange={setDOBChecked}
-              trackColor={{ false: '#000', true: '#81b0ff' }}
-              thumbColor={DOBChecked ? '#f5dd4b' : '#f4f3f4'}
+              trackColor={{ false: '#000', true: '#71d444' }}
+              thumbColor={DOBChecked ? 'white' : 'white'}
             />
             <Text style={localStyles.label}>Date of Birth</Text>
           </View>
@@ -146,8 +128,8 @@ const RequestCredentialScreen = ({ navigation }) => {
             <Switch
               value={photocardChecked}
               onValueChange={togglePhotocardSwitch}
-              trackColor={{ false: '#000', true: '#81b0ff' }}
-              thumbColor={photocardChecked ? '#f5dd4b' : '#f4f3f4'}
+              trackColor={{ false: '#000', true: '#71d444' }}
+              thumbColor={photocardChecked ? 'white' : 'white'}
             />
             <Text style={localStyles.label}>Photocard</Text>
           </View>
@@ -155,8 +137,8 @@ const RequestCredentialScreen = ({ navigation }) => {
             <Switch
               value={driversLicenseChecked}
               onValueChange={toggleDriversLicenseSwitch}
-              trackColor={{ false: '#000', true: '#81b0ff' }}
-              thumbColor={driversLicenseChecked ? '#f5dd4b' : '#f4f3f4'}
+              trackColor={{ false: '#000', true: '#71d444' }}
+              thumbColor={driversLicenseChecked ? 'white' : 'white'}
             />
             <Text style={localStyles.label}>Driver's License</Text>
           </View>
