ada878b1fe1afff5232bb5c690e7e94a6e3933b7
Alex Blackmore <ablackmore113@gmail.com>
2024-08-04 02:33:22 +1000

    remove integration tests from pipeline (again)

--- services/jest.config.js
+++ services/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "service_provider/node_modules/(?!@digitalbazzar)/",
   ],
   modulePathIgnorePatterns: [
-    // "__tests__/integration.ts",
+    "__tests__/integration.ts",
     "__tests__/verifier_presentation.ts",
     "__tests__/wallet_authorization.ts",
     "__tests__/issuer.ts"
