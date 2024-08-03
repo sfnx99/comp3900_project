c62570d4fb31b8c08c028363915e56b821cdba14
BuildTools <unconfigured@null.spigotmc.org>
2024-08-03 14:09:51 +1000

    Fixed pipeline

--- services/jest.config.js
+++ services/jest.config.js
@@ -4,5 +4,11 @@ module.exports = {
   testEnvironment: 'node',
   transformIgnorePatterns: [
     "service_provider/node_modules/(?!@digitalbazzar)/",
+  ],
+  modulePathIgnorePatterns: [
+    "__tests__/integration.ts",
+    "__tests__/verifier_presentation.ts",
+    "__tests__/wallet_authorization.ts",
+    "__tests__/issuer.ts"
   ]
 };
