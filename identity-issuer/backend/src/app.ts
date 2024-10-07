a03d8471974ea3b6a3caa8f72fe88bfd578e176e
Joshua Louttit <jlouttit@outlook.com>
2024-10-07 15:14:00 +1100

    included key generation as part of the initialisation of the server

--- identity-issuer/backend/src/app.ts
+++ identity-issuer/backend/src/app.ts
@@ -1,15 +1,20 @@
 import express from 'express'
 import config from 'config'
 import routes from './routes'
+import {
+    generateIssuerKeys
+} from './utils/keyManagement'
 
 const port = config.get<number>('port')
 
 const app = express()
 app.use(express.json())
 
+// Backend Servers Creation
 routes(app)
-
+// Important to note: This server is AN issuer not mulitple.
 const server = app.listen(port, async () => {
+    generateIssuerKeys()
     console.log(`Identity Issuer Backend is running at http://localhost:${port}`)
 })
 
